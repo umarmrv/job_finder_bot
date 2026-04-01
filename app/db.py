4e841e7fdb2c3929b52684658c885571d0d3a2c8
Baxromjon <b0490624@gmail.com>
2026-04-01 16:42:48 +0500

    changed db .py and removed db url and db password to the dot env so every body should create thier own env settings

--- app/db.py
+++ app/db.py
@@ -1,13 +1,21 @@
+import os
 from typing import AsyncGenerator
-
+from dotenv import load_dotenv  # Добавьте этот импорт
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
 
-DATABASE_URL = "postgresql+asyncpg://shohjahonmurodov@127.0.0.1:5432/jobs_bot"
+# Загружаем переменные из файла .env
+load_dotenv()
+
+# Получаем URL из переменной окружения
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# Проверка, что переменная загрузилась, чтобы избежать ошибки "Could not parse"
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL не найден в файле .env или переменных окружения")
 
 class Base(DeclarativeBase):
     pass
-
 
 engine = create_async_engine(
     DATABASE_URL,
@@ -19,7 +27,6 @@ AsyncSessionLocal = async_sessionmaker(
     class_=AsyncSession,
     expire_on_commit=False,
 )
-
 
 async def get_db() -> AsyncGenerator[AsyncSession, None]:
     async with AsyncSessionLocal() as session:
